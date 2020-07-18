9b2cd7f22d86ad70944fd3714a984bb02b7946be
karansantra <karansantra@192.168.0.104>
2020-07-18 13:42:09 +0530

    created the basic skeleton structure as per the given problem statement

---
+++ DistributingChocolates/src/FirstTask/DistributingChocolates.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FirstTask;
+
+/**
+ *
+ * @author karansantra
+ */
+public class DistributingChocolates {
+
+    private int noOfChocolates;
+    private int noOfStudents;
+    private int minNoOfChocolateLeft;
+
+    public int getNoOfChocolates() {
+        return noOfChocolates;
+    }
+
+    public void setNoOfChocolates(int noOfChocolates) {
+        this.noOfChocolates = noOfChocolates;
+    }
+
+    public int getNoOfStudents() {
+        return noOfStudents;
+    }
+
+    public void setNoOfStudents(int noOfStudents) {
+        this.noOfStudents = noOfStudents;
+    }
+
+    public int getMinNoOfChocolateLeft() {
+        return minNoOfChocolateLeft;
+    }
+
+    public void setMinNoOfChocolateLeft(int minNoOfChocolateLeft) {
+        this.minNoOfChocolateLeft = minNoOfChocolateLeft;
+    }
+
+    public void calculateMinimumNoOfChocolatesLeft() {
+        //do calculation here
+    }
+
+}
